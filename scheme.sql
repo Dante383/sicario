99bdfb210e41df241e2a540926edb6d828b79daf
Peter <dante383@protonmail.com>
2018-05-16 18:17:55 +0200

    database structure

--- scheme.sql
+++ scheme.sql
@@ -1,49 +1,80 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Baza danych: `sicario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `clients`
+--
 
 CREATE TABLE `clients` (
   `id` int(11) NOT NULL,
   `userkey` varchar(32) NOT NULL,
   `ip` varchar(16) NOT NULL,
+  `architecture` varchar(8) DEFAULT NULL,
+  `system` varchar(16) DEFAULT NULL,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `notes` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `clients` (`id`, `userkey`, `ip`, `created_on`, `updated_on`, `notes`) VALUES
-(1, '1E0986F48DD200NCB8D41D13CD926634', '133.7.14.48', '2001-09-11 08:46:20', '2001-09-11 10:28:02', '');
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `jobs`
+--
 
 CREATE TABLE `jobs` (
   `id` int(11) NOT NULL,
   `userkey` varchar(32) NOT NULL,
   `type` varchar(16) NOT NULL,
-  `processed` int(11) NOT NULL,
+  `processed` int(11) NOT NULL DEFAULT '0',
   `payload` text NOT NULL,
   `result` text,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `jobs` (`id`, `userkey`, `type`, `processed`, `payload`, `result`, `created_on`, `updated_on`) VALUES
-(1, '1E0986F48DD200BCB8D41D13CD926634', 'execute', 1, 'whoami', 'Al-Qaeda', '2001-09-11 08:46:20', '2001-09-11 10:28:02');
+--
+-- Indeksy dla zrzutów tabel
+--
 
+--
+-- Indexes for table `clients`
+--
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `jobs`
+--
 ALTER TABLE `jobs`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT dla tabeli `clients`
+--
 ALTER TABLE `clients`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT dla tabeli `jobs`
+--
 ALTER TABLE `jobs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
